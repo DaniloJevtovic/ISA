b82eabf107920c5a593ecbb7ef1290c56e8b46d6
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-06 22:19:12 +0200

    svi kontroleri korisnika ubaceni u jednu klasu

--- src/main/java/com/isa/projekat/controller/UserController.java
+++ src/main/java/com/isa/projekat/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.isa.projekat.controller;
 
-import com.isa.projekat.service.UserService;
-
-import com.isa.projekat.model.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,15 +10,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.isa.projekat.model.User;
+import com.isa.projekat.model.UserDto;
+import com.isa.projekat.service.UserService;
+
 @RestController
 @RequestMapping("/api/users")
-public class LoginController {
-
+public class UserController {
+	
 	@Autowired
 	UserService userService;
 	
@@ -54,5 +58,40 @@ public class LoginController {
 		UserDto loggedDTO = null;
 		return new ResponseEntity<UserDto>(loggedDTO,HttpStatus.OK);
 	}
+	
+	@RequestMapping(value = "/register",
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+			)
+	public ResponseEntity<UserDto> registerUser(@RequestBody User user) throws Exception{
+		userService.save(user);
+		User savedUser = userService.findByEmail(user.getEmail());
+		userService.sendVerificationMail(savedUser);
+		UserDto userdto = new UserDto(user);
+		return new ResponseEntity<UserDto>(userdto, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/verify/{id}", method = RequestMethod.GET)
+	public ResponseEntity<String> verifyUser(@PathVariable Long id) throws Exception{
+		userService.verifyEmail(id);
+		return new ResponseEntity<String>("Uspjesno ste verifikovani!", HttpStatus.ACCEPTED);
+	}
 
+	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<UserDto> editedUser(@RequestBody User user, @PathVariable Long id){
+		User editedUser = userService.editUser(user, id);
+		return new ResponseEntity<UserDto>(new UserDto(editedUser),HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDto>> getUsers(){
+		List<User> users = userService.findAll();
+		List<UserDto> usersdto = new ArrayList<UserDto>();
+		for(User user : users) {
+			usersdto.add(new UserDto(user));
+		}
+		return new ResponseEntity<List<UserDto>>(usersdto,HttpStatus.FOUND);
+	}
+	
 }
