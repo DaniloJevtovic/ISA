999399c324485139f09e0074d01a2fb4b56419bf
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-11 00:23:10 +0200

    ispravljena greska, dodati novi podaci

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,9 @@ insert into User (user_name, user_surname, user_email, user_password, user_phone
 insert into User (user_name, user_surname, user_email, user_password, user_phone, user_city, user_type, user_verified) values ('Stefa', 'Safari', 'stefa@hotmail.com', 'stefa12', '336-342', 'bbb', '0', TRUE)
 insert into User (user_name, user_surname, user_email, user_password, user_phone, user_city, user_type, user_verified) values ('Govece', 'Jeftanovic', 'govece@hotmail.com', 'govece12', '325-234', 'sss', '2', TRUE)
 
-insert into CinemaTheatre (ct_name, ct_adress, ct_description, ct_type) values ('SNP', 'Pozorisni trg 1', 'Srpsko narodno pozoriste', '1')
+insert into cinema_theatre (ct_name, ct_adress, ct_description, ct_type) values ('SNP', 'Pozorisni trg 1', 'Srpsko narodno pozoriste', '1')
+insert into cinema_theatre (ct_name, ct_adress, ct_description, ct_type) values ('Arena Cinaplex', 'Bulevar Mihajla Pupina 3', 'Opis...', '0')
+
+insert into movie_show (ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('Dumb and dumber', '0', 'opis...', 'Jim Carrey, Jeff Daniels', '1.47', '7.3', 'Bobby Farrelly, Peter Farrelly', 'poster' )
+insert into movie_show (ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('Nueve reinas', '0', 'opis...', ' Ricardo Darín, Gastón Pauls', '1.54', '7.9', 'Fabian Bielinsky', 'poster' )
+insert into movie_show (ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('Contratiempo', '0', 'opis...', '  Mario Casas, Ana Wagener', '1.46', '8.1', 'Oriol Paulo', 'poster' )
