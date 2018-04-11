5f5c271fc21dca1a91db0646f837ca2a97085a3e
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-11 18:22:29 +0200

    dodati modeli za sale i sjedista

---
+++ src/main/java/com/isa/projekat/model/Hall.java
@@ -0,0 +1,84 @@
+package com.isa.projekat.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Hall implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="hall_number", nullable = false)
+	private int hallNumber;
+	
+	@Column(name="hall_rows", nullable = false)
+	private int hallRows;
+	
+	@ManyToOne(targetEntity = CinemaTheatre.class)
+	private CinemaTheatre cinemaTheatre;
+	
+	@OneToMany(targetEntity = HallSeat.class)
+	private List<HallSeat> hallSeats;
+
+	public Hall(Long id, int hallNumber, int hallRows, CinemaTheatre cinemaTheatre, List<HallSeat> hallSeats) {
+		super();
+		this.id = id;
+		this.hallNumber = hallNumber;
+		this.hallRows = hallRows;
+		this.cinemaTheatre = cinemaTheatre;
+		this.hallSeats = hallSeats;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getHallNumber() {
+		return hallNumber;
+	}
+
+	public void setHallNumber(int hallNumber) {
+		this.hallNumber = hallNumber;
+	}
+
+	public int getHallRows() {
+		return hallRows;
+	}
+
+	public void setHallRows(int hallRows) {
+		this.hallRows = hallRows;
+	}
+
+	public CinemaTheatre getCinemaTheatre() {
+		return cinemaTheatre;
+	}
+
+	public void setCinemaTheatre(CinemaTheatre cinemaTheatre) {
+		this.cinemaTheatre = cinemaTheatre;
+	}
+
+	public List<HallSeat> getHallSeats() {
+		return hallSeats;
+	}
+
+	public void setHallSeats(List<HallSeat> hallSeats) {
+		this.hallSeats = hallSeats;
+	}
+
+}
---
+++ src/main/java/com/isa/projekat/model/HallSeat.java
@@ -0,0 +1,83 @@
+package com.isa.projekat.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class HallSeat implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="seat_number", nullable = false)
+	private int seatNumber;
+
+	@Column(name="seat_row", nullable = false)
+	private int seatRow;
+	
+	@ManyToOne
+	private Hall hall;
+	
+	@ManyToOne
+	private Projection projection;
+
+	public HallSeat(Long id, int seatNumber, int seatRow, Hall hall, Projection projection) {
+		super();
+		this.id = id;
+		this.seatNumber = seatNumber;
+		this.seatRow = seatRow;
+		this.hall = hall;
+		this.projection = projection;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+
+	public void setSeatNumber(int seatNumber) {
+		this.seatNumber = seatNumber;
+	}
+
+	public int getSeatRow() {
+		return seatRow;
+	}
+
+	public void setSeatRow(int seatRow) {
+		this.seatRow = seatRow;
+	}
+
+	public Hall getHall() {
+		return hall;
+	}
+
+	public void setHall(Hall hall) {
+		this.hall = hall;
+	}
+
+	public Projection getProjection() {
+		return projection;
+	}
+
+	public void setProjection(Projection projection) {
+		this.projection = projection;
+	}
+
+	
+}
