70b32f8c1f863b7544f782391b70a54a600e1e7c
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-11 18:21:52 +0200

    dodat model za repertoar

---
+++ src/main/java/com/isa/projekat/model/Repertoire.java
@@ -0,0 +1,63 @@
+package com.isa.projekat.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class Repertoire implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+	@JsonBackReference
+	private CinemaTheatre cinemaTheatre;
+	
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "repertoire")
+	private List<MovieShow> movieShows;
+
+	public Repertoire(Long id, CinemaTheatre cinemaTheatre, List<MovieShow> movieShows) {
+		super();
+		this.id = id;
+		this.cinemaTheatre = cinemaTheatre;
+		this.movieShows = movieShows;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public CinemaTheatre getCinemaTheatre() {
+		return cinemaTheatre;
+	}
+
+	public void setCinemaTheatre(CinemaTheatre cinemaTheatre) {
+		this.cinemaTheatre = cinemaTheatre;
+	}
+
+	public List<MovieShow> getMovieShows() {
+		return movieShows;
+	}
+
+	public void setMovieShows(List<MovieShow> movieShows) {
+		this.movieShows = movieShows;
+	}
+
+}
