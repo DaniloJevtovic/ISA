17026ae0e684dcd5497e598cf229a25e61273c71
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-11 18:28:44 +0200

    male izmjene

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,6 @@ insert into User (user_name, user_surname, user_email, user_password, user_phone
 insert into cinema_theatre (ct_name, ct_adress, ct_description, ct_type) values ('SNP', 'Pozorisni trg 1', 'Srpsko narodno pozoriste', '1')
 insert into cinema_theatre (ct_name, ct_adress, ct_description, ct_type) values ('Arena Cinaplex', 'Bulevar Mihajla Pupina 3', 'Opis...', '0')
 
-insert into movie_show (ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('Dumb and dumber', '0', 'opis...', 'Jim Carrey, Jeff Daniels', '1.47', '7.3', 'Bobby Farrelly, Peter Farrelly', 'poster' )
-insert into movie_show (ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('Nueve reinas', '0', 'opis...', ' Ricardo Darín, Gastón Pauls', '1.54', '7.9', 'Fabian Bielinsky', 'poster' )
-insert into movie_show (ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('Contratiempo', '0', 'opis...', '  Mario Casas, Ana Wagener', '1.46', '8.1', 'Oriol Paulo', 'poster' )
+insert into movie_show (ms_type, ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('0', 'Dumb and dumber', '0', 'opis...', 'Jim Carrey, Jeff Daniels', '1.47', '7.3', 'Bobby Farrelly, Peter Farrelly', 'poster' )
+insert into movie_show (ms_type, ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('0', 'Nueve reinas', '0', 'opis...', ' Ricardo Darín, Gastón Pauls', '1.54', '7.9', 'Fabian Bielinsky', 'poster' )
+insert into movie_show (ms_type, ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster ) values ('0', 'Contratiempo', '0', 'opis...', '  Mario Casas, Ana Wagener', '1.46', '8.1', 'Oriol Paulo', 'poster' )
