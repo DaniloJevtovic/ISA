d7e7534bf0fbcfa26d676fe084ed3423da8938f6
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-12 17:39:33 +0200

    dodata tabela za prikaz svih bioskopa

---
+++ src/main/resources/static/js/cinemas.js
@@ -0,0 +1,25 @@
+var cinemas="../api/cinemastheatres/getCinemas"
+	
+	function getCinemas(){
+	$.ajax({
+		 url: cinemas,
+		 method: "GET",
+		 success: function(data){
+			 $(".cinemasTable").empty();
+			 for(i=0;i<data.length;i++){
+				 $(".cinemasTable").append(`<tr>
+                              <td>`+data[i].name+`</td>
+                              <td>`+data[i].adress+`</td>
+                              <td>`+data[i].description+`</td>
+                              <td align="center"><button type="button" class="btn btn-primary btn-rounded btn-sm my-0">Pogledaj</button></td>
+                          </tr>`);
+			 }
+		 },
+		 error: function(){
+			 alert("Greska");
+		 }
+	});
+	
+	}
+	
+	
