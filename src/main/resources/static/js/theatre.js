2f38edf24d8b0b70a141ea7895ed36e41427f591
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-12 17:40:06 +0200

    male izmjene

---
+++ src/main/resources/static/js/theatre.js
@@ -0,0 +1,22 @@
+var theaters="../api/cinemastheatres/getTheaters"
+	
+	function getTheaters(){
+	$.ajax({
+		 url: theaters,
+		 method: "GET",
+		 success: function(data){
+			 $(".theatersTable").empty();
+			 for(i=0;i<data.length;i++){
+				 $(".theatersTable").append(`<tr>
+						 	<td>`+data[i].name+`</td>
+                            <td>`+data[i].adress+`</td>
+                              <td>`+data[i].description+`</td>
+                              <td align="center"><button type="button" class="btn btn-primary btn-rounded btn-sm my-0">Pogledaj</button></td>
+                           </tr>`);
+			 }
+		 },
+		 error: function(){
+			 alert("Greska");
+		 }
+	});
+}
