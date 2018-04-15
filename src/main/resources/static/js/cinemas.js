4b7f75db236b0d387d26347632c628a4a4152a19
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-15 03:24:49 +0200

    prikaz filmova u repertoaru

--- src/main/resources/static/js/cinemas.js
+++ src/main/resources/static/js/cinemas.js
@@ -11,7 +11,7 @@ var cinemas="../api/cinemastheatres/getCinemas"
                               <td>`+data[i].name+`</td>
                               <td>`+data[i].adress+`</td>
                               <td>`+data[i].description+`</td>
-                              <td align="center"><button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#cinemaModal">Pogledaj</button></td>
+                              <td align="center"><button type="button" onclick="generateRepertoire(`+data[i].id+`)" id=`+data[i].id+` class="btn btn-info btn-sm" data-toggle="modal" data-target="#cinemaModal">Pogledaj</button></td>
                           </tr>`);
 			 }
 		 },
@@ -19,6 +19,24 @@ var cinemas="../api/cinemastheatres/getCinemas"
 			 alert("Greska");
 		 }
 	});
+	
+	}
+
+	function generateRepertoire(id){
+		$.ajax({
+			url: "../api/cinemastheatres/" +id+ "/getMoviesShows",
+			method: "GET",
+			success: function(data){
+				$("#movies").empty();
+				for(i=0; i<data.length; i++){
+					$("#movies").append(`<p id=`+data[i].id+`> Naziv: `+data[i].name+` <br> Zanr: `+data[i].genre+` 
+					<br> Trajanje: `+data[i].duration+`  <br> Rejting: `+data[i].rating+` <hr> </p>`);
+				}
+			},
+			error: function(){
+				alert("greska greska greska");
+			}
+		});
 	
 	}
 	
