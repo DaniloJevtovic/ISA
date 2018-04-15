c7f203041376a674afce25ff67e48dfb6d14df41
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-15 03:23:10 +0200

    povezani filmovi sa bioskopom

--- src/main/java/com/isa/projekat/controller/CinemaTheatreController.java
+++ src/main/java/com/isa/projekat/controller/CinemaTheatreController.java
@@ -6,12 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isa.projekat.model.CinemaTheatre;
 import com.isa.projekat.model.CinemaTheatreDto;
+import com.isa.projekat.model.MovieShow;
+import com.isa.projekat.model.MovieShowDto;
 import com.isa.projekat.service.CinemaTheatreService;
 
 @RestController
@@ -38,5 +41,14 @@ public class CinemaTheatreController {
 		}
 		return new ResponseEntity<List<CinemaTheatreDto>>(cinemasDto, HttpStatus.OK);
 	}
-		
+	
+	@RequestMapping(value="/{ctId}/getMoviesShows",method = RequestMethod.GET)
+	public ResponseEntity<List<MovieShowDto>> getMoviesShows(@PathVariable Long ctId){
+		List<MovieShowDto> movieShowDtos = new ArrayList<MovieShowDto>();
+		for(MovieShow movieShow : cinemaTheatreService.getMovieShows(ctId)){
+			movieShowDtos.add(new MovieShowDto(movieShow));
+		}
+		return new ResponseEntity<List<MovieShowDto>>(movieShowDtos,HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/isa/projekat/controller/MovieShowController.java
+++ src/main/java/com/isa/projekat/controller/MovieShowController.java
@@ -1,16 +1,18 @@
 package com.isa.projekat.controller;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isa.projekat.model.MovieShow;
+import com.isa.projekat.model.MovieShowDto;
 import com.isa.projekat.service.MovieShowService;
 
 @RestController
@@ -20,14 +22,20 @@ public class MovieShowController {
 	@Autowired
 	private MovieShowService movieShowService;
 	
-	@RequestMapping(
-			value = "/getMovies",
-			method = RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Collection<MovieShow>> getMovies(){
-		
-		return new ResponseEntity<Collection<MovieShow>>(movieShowService.findAll(), HttpStatus.OK);
-		
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<MovieShowDto>> getAllMoviesShows(){
+		List<MovieShow> movieShows = movieShowService.findAll();
+		List<MovieShowDto> movieShowDtos = new ArrayList<MovieShowDto>();
+		for(MovieShow movieShow : movieShows){
+			movieShowDtos.add(new MovieShowDto(movieShow));
+		}
+		return new ResponseEntity<List<MovieShowDto>>(movieShowDtos,HttpStatus.OK);
 	}
-
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<MovieShowDto> save(@RequestBody MovieShow movieShow){
+		MovieShowDto movieShowDto = new MovieShowDto(movieShowService.save(movieShow));
+		return new ResponseEntity<MovieShowDto>(movieShowDto, HttpStatus.CREATED);
+	}
+	
 }
