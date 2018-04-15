864129dc0f003ebc2b5b30d525cd3882499afbf9
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-15 03:24:15 +0200

    dodati servisi

--- src/main/java/com/isa/projekat/service/CinemaTheatreService.java
+++ src/main/java/com/isa/projekat/service/CinemaTheatreService.java
@@ -8,6 +8,8 @@ public interface CinemaTheatreService {
 	CinemaTheatre save (CinemaTheatre cinemaTheatre);
 	CinemaTheatre getOne(Long id);
 	
+	List<MovieShow>getMovieShows(Long ctId);
+	
 	List<CinemaTheatre> getAll();
 	List<CinemaTheatre> getAllTheatres();
 	List<CinemaTheatre> getAllCinemas();
--- src/main/java/com/isa/projekat/service/CinemaTheatreServiceImpl.java
+++ src/main/java/com/isa/projekat/service/CinemaTheatreServiceImpl.java
@@ -2,12 +2,14 @@ package com.isa.projekat.service;
 
 import java.util.List;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.isa.projekat.model.CinemaTheatre;
 import com.isa.projekat.model.CinemaTheatreType;
+import com.isa.projekat.model.MovieShow;
 import com.isa.projekat.repository.CinemaTheatreRepository;
 
 @Service
@@ -47,6 +49,12 @@ public class CinemaTheatreServiceImpl implements CinemaTheatreService {
 		return ctRepository.findByType(CinemaTheatreType.CINEMA);
 	}
 
-	
+	@Override
+	public List<MovieShow> getMovieShows(Long ctId) {
+		// TODO Auto-generated method stub
+		CinemaTheatre cinemaTheatre = ctRepository.findOne(ctId);
+		Hibernate.initialize(cinemaTheatre.getMovieShows());
+		return cinemaTheatre.getMovieShows();
+	}
 
 }
