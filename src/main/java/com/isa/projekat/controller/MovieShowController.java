331d8d39de4c62c0d5eba3c8cbc22a6eb4af4242
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-17 04:46:58 +0200

    kontroler za projekciju

--- src/main/java/com/isa/projekat/controller/MovieShowController.java
+++ src/main/java/com/isa/projekat/controller/MovieShowController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.isa.projekat.model.MovieShow;
 import com.isa.projekat.model.MovieShowDto;
+import com.isa.projekat.model.Projection;
+import com.isa.projekat.model.ProjectionDto;
 import com.isa.projekat.service.MovieShowService;
+import com.isa.projekat.service.ProjectionService;
 
 @RestController
 @RequestMapping("/api/movieshows")
@@ -21,6 +25,9 @@ public class MovieShowController {
 
 	@Autowired
 	private MovieShowService movieShowService;
+	
+	@Autowired
+	ProjectionService projectionService;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<MovieShowDto>> getAllMoviesShows(){
@@ -36,6 +43,16 @@ public class MovieShowController {
 	public ResponseEntity<MovieShowDto> save(@RequestBody MovieShow movieShow){
 		MovieShowDto movieShowDto = new MovieShowDto(movieShowService.save(movieShow));
 		return new ResponseEntity<MovieShowDto>(movieShowDto, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/{msId}/projections", method = RequestMethod.GET)
+	public ResponseEntity<List<ProjectionDto>> getProjectionsForMovieShow(@PathVariable Long msId) {
+		List<Projection> projections = projectionService.getProjectionsForMovieShow(msId);
+		List<ProjectionDto> projectionDtos = new ArrayList<ProjectionDto>();
+		for(Projection projection : projections) {
+			projectionDtos.add(new ProjectionDto(projection));
+		}
+		return new ResponseEntity<List<ProjectionDto>>(projectionDtos, HttpStatus.OK);
 	}
 	
 }
