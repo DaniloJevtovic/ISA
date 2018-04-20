3e9631ac4dc5ff32f8d8ddfad1bbcde2cc58c22e
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-04-20 23:41:08 +0200

    servisi vremna projekcije

--- src/main/java/com/isa/projekat/service/ProjectionTimeService.java
+++ src/main/java/com/isa/projekat/service/ProjectionTimeService.java
@@ -2,13 +2,19 @@ package com.isa.projekat.service;
 
 import java.util.List;
 
+import com.isa.projekat.model.HallSeat;
 import com.isa.projekat.model.ProjectionTime;
 import com.isa.projekat.model.Reservation;
 
 public interface ProjectionTimeService {
 
 	ProjectionTime save(ProjectionTime projectionTime);
+
 	List<ProjectionTime> findByProjection(Long projectionId);
+
 	ProjectionTime findOne(Long projectionId);
-	Reservation reserve();
+
+	Reservation reserveSeats(Long projectiontimeId, List<String> seatinfo, Long userId);
+
+	List<HallSeat> getTakenSeats(Long projectiontimeId);
 }
--- src/main/java/com/isa/projekat/service/ProjectionTimeServiceImpl.java
+++ src/main/java/com/isa/projekat/service/ProjectionTimeServiceImpl.java
@@ -1,13 +1,17 @@
 package com.isa.projekat.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.isa.projekat.model.HallSeat;
 import com.isa.projekat.model.Projection;
 import com.isa.projekat.model.ProjectionTime;
 import com.isa.projekat.model.Reservation;
+import com.isa.projekat.model.User;
 import com.isa.projekat.repository.HallSeatRepository;
 import com.isa.projekat.repository.ProjectionRepository;
 import com.isa.projekat.repository.ProjectionTimeRepository;
@@ -19,20 +23,19 @@ public class ProjectionTimeServiceImpl implements ProjectionTimeService {
 
 	@Autowired
 	UserRepository userRepository;
-	
-	@Autowired 
+
+	@Autowired
 	ProjectionRepository projectionRepository;
-	
+
 	@Autowired
 	ReservationRepository reservationRepository;
-	
+
 	@Autowired
 	ProjectionTimeRepository projectionTimeRepository;
-	
-	@Autowired 
+
+	@Autowired
 	HallSeatRepository hallSeatRepository;
-	
-	
+
 	@Override
 	public ProjectionTime save(ProjectionTime projectionTime) {
 		// TODO Auto-generated method stub
@@ -53,10 +56,34 @@ public class ProjectionTimeServiceImpl implements ProjectionTimeService {
 	}
 
 	@Override
-	public Reservation reserve() {
+	public Reservation reserveSeats(Long projectiontimeId, List<String> seatinfo, Long userId) {
 		// TODO Auto-generated method stub
-		return null;
+		ProjectionTime projectionTime = projectionTimeRepository.findOne(projectiontimeId);
+		Hibernate.initialize(projectionTime.getHallSeats());
+		List<HallSeat> resSeats = new ArrayList<HallSeat>();
+		for (int i = 0; i < seatinfo.size(); i++) {
+			String arr[] = seatinfo.get(i).split("_");
+			HallSeat seat = hallSeatRepository.findSeatByHallAndRowAndNumber(projectionTime.getHall(),
+					Integer.parseInt(arr[0]), Integer.parseInt(arr[1]));
+			resSeats.add(seat);
+			projectionTime.getHallSeats().add(seat);
+		}
+		projectionTimeRepository.save(projectionTime);
+		User user = userRepository.findOne(userId);
+		Reservation reservation = new Reservation(user, projectionTime, resSeats);
+		reservationRepository.save(reservation);
+		Hibernate.initialize(user.getReservations());
+		user.getReservations().add(reservation);
+		userRepository.save(user);
+		return reservation;
 	}
 
+	@Override
+	public List<HallSeat> getTakenSeats(Long projectiontimeId) {
+		// TODO Auto-generated method stub
+		ProjectionTime projectionTime = projectionTimeRepository.findOne(projectiontimeId);
+		Hibernate.initialize(projectionTime.getHallSeats());
+		return projectionTime.getHallSeats();
+	}
 
 }
