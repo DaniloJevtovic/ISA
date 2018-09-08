9b03c5aed576dc7f91a1ee23659f42fdccad08a3
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2018-09-08 02:50:39 +0200

    ipak je lakse sa upitima

--- src/main/java/com/isa/projekat/Data.java
+++ src/main/java/com/isa/projekat/Data.java
@@ -1,15 +1,28 @@
 package com.isa.projekat;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import javax.annotation.PostConstruct;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.isa.projekat.model.CinemaTheatre;
+import com.isa.projekat.model.CinemaTheatreType;
+import com.isa.projekat.model.Hall;
+import com.isa.projekat.model.MovieShow;
+import com.isa.projekat.model.MovieShowGenre;
+import com.isa.projekat.model.MovieShowType;
+import com.isa.projekat.model.Projection;
 import com.isa.projekat.model.User;
 import com.isa.projekat.model.UserType;
-
+import com.isa.projekat.service.CinemaTheatreService;
+import com.isa.projekat.service.HallService;
+import com.isa.projekat.service.MovieShowService;
+import com.isa.projekat.service.ProjectionService;
 import com.isa.projekat.service.UserService;
 
 @Component
@@ -17,6 +30,18 @@ public class Data {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private CinemaTheatreService ctService;
+	
+	@Autowired
+	private HallService hallService;
+	
+	@Autowired
+	private MovieShowService msService;
+	
+	@Autowired
+	private ProjectionService prService;
 
 	@PostConstruct
 	private void add() throws ParseException {
@@ -31,7 +56,105 @@ public class Data {
 		user.setUserType(UserType.REGISTROVAN);
 		user.setVerified(true);
 		userService.save(user);
+		
+		Hall sala1CK = new Hall();
+		sala1CK.setNumber(1);
+		sala1CK.setRows(2);	//2 reda
+		sala1CK.setSeatsPerRow(5);	//4 sjedista u redu (kolone)
+		
+		Hall sala2CK = new Hall();
+		sala2CK.setNumber(2);
+		sala2CK.setRows(3);	//3 reda - 3 vrste
+		sala1CK.setSeatsPerRow(8);	//8 sjedista u redu (kolone)
+		
+		hallService.save(sala1CK);
+		hallService.save(sala2CK);
+		
+		Projection prCK1 = new Projection();
+		prCK1.setDate(new SimpleDateFormat("yyyy-MM-dd").parse("2018-04-27"));
+		prService.save(prCK1);
+		
+		Projection prCK2 = new Projection();
+		prCK2.setDate(new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-27"));
+		prService.save(prCK2);
+		//prCK1.setMovieShow(film1CK);
+		
+		Projection prCK3 = new Projection();
+		prCK3.setDate(new SimpleDateFormat("yyyy-MM-dd").parse("2018-11-13"));
+		prService.save(prCK3);
+		
+		MovieShow film1CK = new MovieShow();
+		film1CK.setName("Fast & Furious");
+		film1CK.setActors("Vin Diesel, Paul Walker, Michelle Rodriguez");
+		film1CK.setDescription("Brian O'Conner, back working for the FBI in Los Angeles, teams up with Dominic Toretto to bring down a heroin importer by infiltrating his operation.");
+		film1CK.setDirector("Justin Lin");
+		film1CK.setDuration("1h 47min");
+		film1CK.setGenre(MovieShowGenre.ACTION);
+		film1CK.setPoster("images/fast.jpg");
+		film1CK.setType(MovieShowType.MOVIE);
+		film1CK.setRating("3");
+		//film1CK.setCinemaTheatre(centarZaKulturu);
+		msService.save(film1CK);
+		
+		MovieShow film2CK = new MovieShow();
+		film2CK.setName("Tesko je biti fin");
+		film2CK.setActors("Sasa Petrovic, Daria Lorenci, Damir Savic");
+		film2CK.setDescription("Taxidriver Fudo wants a happy life with his wife an son. But he has depts. He decides to plan a last coup with his gangster friends.");
+		film2CK.setDirector("Srdjan Vuletic");
+		film2CK.setDuration("1h 42min");
+		film2CK.setGenre(MovieShowGenre.DRAMA);
+		film2CK.setPoster("images/tesko.jpg");
+		film2CK.setType(MovieShowType.MOVIE);
+		film2CK.setRating("5");
+		//film1CK.setCinemaTheatre(centarZaKulturu);
+		
+		film1CK.setProjections(new ArrayList<Projection>());
+		film1CK.getProjections().add(prCK1);
+		
+		msService.save(film2CK);
+		
+		
+		
+		CinemaTheatre centarZaKulturu = new CinemaTheatre();
+		centarZaKulturu.setName("Centar za kulturu");
+		centarZaKulturu.setAdress("Trg pravosljavlja");
+		centarZaKulturu.setDescription("Nalazi se u derventi");
+		centarZaKulturu.setType(CinemaTheatreType.CINEMA);
+		centarZaKulturu.setGrade("4");
 
+
+		centarZaKulturu.setMovieShows(new ArrayList<MovieShow>());
+		centarZaKulturu.getMovieShows().add(film1CK);
+		centarZaKulturu.getMovieShows().add(film2CK);
+		
+		
+		ctService.save(centarZaKulturu);
+		
+		Hibernate.initialize(film1CK.getCinemaTheatre());
+		film1CK.setCinemaTheatre(centarZaKulturu);
+		msService.save(film1CK);
+		
+		Hibernate.initialize(film2CK.getCinemaTheatre());
+		film2CK.setCinemaTheatre(centarZaKulturu);
+		msService.save(film2CK);
+		
+		Hibernate.initialize(prCK1.getMovieShow());
+		prCK1.setMovieShow(film1CK);
+		prService.save(prCK1);
+		
+		Hibernate.initialize(prCK1.getMovieShow());
+		prCK3.setMovieShow(film1CK);
+		prService.save(prCK3);
+		
+		Hibernate.initialize(prCK2.getMovieShow());
+		prCK2.setMovieShow(film2CK);
+		prService.save(prCK2);
+
+		
+		
+		
+		
+		
 	}
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,22 +21,29 @@ insert into movie_show (ms_type, ms_name, ms_genre, ms_description, ms_actors, m
 insert into movie_show (ms_type, ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster, ms_repertoire, cinema_theatre_id ) values ('0', 'Nueve reinas', '3', 'opis...', ' Ricardo Darín, Gastón Pauls', '1.54', '7.9', 'Fabian Bielinsky', 'images/neuve.jpg', 1, 4 )
 insert into movie_show (ms_type, ms_name, ms_genre, ms_description, ms_actors, ms_duration, ms_rating, ms_director, ms_poster, ms_repertoire, cinema_theatre_id ) values ('0', 'Contratiempo', '4', 'opis...', '  Mario Casas, Ana Wagener', '1.46', '8.1', 'Oriol Paulo', 'images/contratiempo.jpg', 1, 4 )
 
+
 --repertoar za bioskope
 insert into cinema_theatre_movie_shows(cinema_theatre_id, movie_shows_id) values (4, 1)
 insert into cinema_theatre_movie_shows(cinema_theatre_id, movie_shows_id) values (4, 2)
 insert into cinema_theatre_movie_shows(cinema_theatre_id, movie_shows_id) values (4, 3)
 
---sale
-insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('1', 20, 10, 4)
-insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('2', 50, 20, 4)
-insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('3', 20, 10, 4)
+--sale za arena cinaplex
+insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('1', 4, 10, 4)	
+insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('2', 2, 2, 4)	
+insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('3', 5, 5, 4)
+insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('4', 5, 5, 4)
+insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('5', 5, 5, 4)
+insert into hall (hall_number, hall_rows, hall_seatrows, cinema_theatre_id) values ('6', 5, 5, 4)
 
---sala-kino
-insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (1, 1)
-insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (1, 2)
-insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (1, 3)
+--sala-kino	arena cinaplex
+insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (4, 1)
+insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (4, 2)
+insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (4, 3)
+insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (4, 4)
+insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (4, 5)
+insert into cinema_theatre_halls (cinema_theatre_id, halls_id) values (4, 6)
 
---projekcije za prvi film
+--projekcije za prvi film (datum projekcije)
 insert into projection (prj_date, movie_show_id) values ('2018-04-27', '1')
 insert into projection (prj_date, movie_show_id) values ('2017-04-12', '1')
 insert into projection (prj_date, movie_show_id) values ('2011-04-12', '1')
@@ -47,27 +54,142 @@ insert into projection (prj_date, movie_show_id) values ('2012-04-12', '2')
 --projekcije za treci film
 insert into projection (prj_date, movie_show_id) values ('2011-04-12', '3')
 
---projekcija-film
+--projekcija-film - prvi film (datum projekcije) - arena cinaplex
 insert into movie_show_projections (movie_show_id, projections_id) values (1, 1)
+insert into movie_show_projections (movie_show_id, projections_id) values (1, 2)
+insert into movie_show_projections (movie_show_id, projections_id) values (1, 3)
+insert into movie_show_projections (movie_show_id, projections_id) values (1, 4)
+--projekcija-film - drugi film film (datum projekcije) - arena cinaplex
+insert into movie_show_projections (movie_show_id, projections_id) values (2, 5)
+insert into movie_show_projections (movie_show_id, projections_id) values (2, 6)
+--projekcija-film -treci ifilm (datum projekcije) - arena cinaplex
+insert into movie_show_projections (movie_show_id, projections_id) values (3, 7)
 
---sjediste
-insert into hall_seat (seat_number, seat_row, hall_id) values (4, 15, 1)		--br sjedista 4, red 5.
 
---sjediste sala
---insert into hall_hall_seats (hall_id, hall_seats_id) values (1, 1)
+--sjediste -- broj sjedista u redu/ broj reda u kojem se sjediste nalazi
+--SJEDISTAAA za salu 4x10 = 40 upita :(
+--prvi red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 1, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 1, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 1, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 1, 1)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 1, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (6, 1, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (7, 1, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (8, 1, 1)
+insert into hall_seat (seat_number, seat_row, hall_id) values (9, 1, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (10, 1, 1)	
+--drugi red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 2, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 3, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 2, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 2, 1)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 2, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (6, 2, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (7, 2, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (8, 2, 1)
+insert into hall_seat (seat_number, seat_row, hall_id) values (9, 2, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (10, 2, 1)
+--treci red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 3, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 3, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 3, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 3, 1)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 3, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (6, 3, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (7, 3, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (8, 3, 1)
+insert into hall_seat (seat_number, seat_row, hall_id) values (9, 3, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (10, 3, 1)	
+--cetvrti red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 5, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 5, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 5, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 5, 1)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 5, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (6, 5, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (7, 5, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (8, 5, 1)
+insert into hall_seat (seat_number, seat_row, hall_id) values (9, 5, 1)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (10, 5, 1)	
 
---vrijeme projekcije
-insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('16:30', '200', 1, 1)
+
+--sejdista za salu broj 2 -  2x2
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 1, 2)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 1, 2)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 2, 2)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 2, 2)
+
+--sjedista za salu broj 3 - 5x5
+--prvi red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 1, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 1, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 1, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 1, 3)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 1, 3)
+--drugi red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 2, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 2, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 2, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 2, 3)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 2, 3)
+--treci red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 3, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 3, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 3, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 3, 3)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 3, 3)
+--cetvrti red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 4, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 4, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 4, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 4, 3)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 4, 3)
+--peti red
+insert into hall_seat (seat_number, seat_row, hall_id) values (1, 5, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (2, 5, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (3, 5, 3)	
+insert into hall_seat (seat_number, seat_row, hall_id) values (4, 5, 3)
+insert into hall_seat (seat_number, seat_row, hall_id) values (5, 5, 3)
+
+
+
+--vrijeme projekcije - prva sala prvi datum
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('16:30', '150', 1, 1)
 insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('18:00', '200', 1, 1)
-insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('18:45', '200', 1, 1)
-insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('20:00', '350', 1, 1)
 insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('21:15', '400', 1, 1)
+
+--vrijeme projekcije - druga sala drugi datum
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('18:45', '250', 2, 2)
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('20:00', '350', 2, 2)
+
+--vrijeme projekcije - treca sala treci datum
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('18:45', '250', 3, 3)
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('20:00', '350', 3, 3)
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('20:45', '250', 3, 3)
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('21:00', '350', 3, 3)
+
+--moze i ovo
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('22:00', '350', 1, 3)
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('22:00', '350', 2, 1)
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('22:00', '350', 3, 2)
+insert into projection_time (pt_time, pt_price, hall_id, projection_id) values ('22:00', '350', 1, 3)
+
 
 --projekcija, vrijme projekcije
 insert into projection_projection_times (projection_id, projection_times_id) values (1, 1)
 insert into projection_projection_times (projection_id, projection_times_id) values (1, 2)
 insert into projection_projection_times (projection_id, projection_times_id) values (1, 3)
 
+insert into projection_projection_times (projection_id, projection_times_id) values (2, 4)
+insert into projection_projection_times (projection_id, projection_times_id) values (2, 5)
+
+insert into projection_projection_times (projection_id, projection_times_id) values (3, 6)
+insert into projection_projection_times (projection_id, projection_times_id) values (3, 7)
+insert into projection_projection_times (projection_id, projection_times_id) values (3, 8)
+insert into projection_projection_times (projection_id, projection_times_id) values (3, 9)
+
 --insert into projection_time_hall_seats (projection_time_id, hall_seats_id) values (1, 1)
 
 --insert into reservation (user_id, projection_time_id, price, visited) values (1, 1, 200, 0)
+
